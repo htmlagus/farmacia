f73666f0890c6a64d84f72ef8349d288bfd7561c
Martin8398 <martinnicolas.larrosa@gmail.com>
2024-10-16 23:56:14 -0300

    soluciono problemas de inicio y deslogeo de sesion

--- app/controllers/auth.controller.php
+++ app/controllers/auth.controller.php
@@ -2,58 +2,68 @@
 require_once 'app/model/user.model.php';
 require_once 'app/view/auth.view.php';
 
-class AuthController {
+class AuthController
+{
 
     private $model;
     private $view;
 
-    function __construct() {
+    function __construct()
+    {
         $this->model = new UsuarioModel();
         $this->view = new AuthView();
     }
 
-function mostrarLogin() {
+    function mostrarLogin()
+    {
 
-    // muestro form de login
-    return $this->view->mostrarLogin();
+        // muestro form de login
+        return $this->view->mostrarLogin();
     }
 
 
-public function login() {
-    if (!isset($_POST['usuario']) || empty($_POST['usuario'])) {
-        return $this->view->mostrarLogin('Falta completar el nombre de usuario');
+    public function login()
+
+
+    {
+        if (!isset($_POST['usuario']) || empty($_POST['usuario'])) {
+            return $this->view->mostrarLogin('Falta completar el nombre de usuario');
+        }
+
+        if (!isset($_POST['contraseña']) || empty($_POST['contraseña'])) {
+            return $this->view->mostrarLogin('Falta completar la contraseña');
+        }
+
+        $usuario = htmlspecialchars($_POST['usuario']);
+        $contraseña = htmlspecialchars($_POST['contraseña']);
+
+        // Verificar que el usuario está en la base de datos
+        $userFromDB = $this->model->obtenerUsuario($usuario);
+
+        // password: 123456
+        // $userFromDB->password: $2y$10$xQop0wF1YJ/dKhZcWDqHceUM96S04u73zGeJtU80a1GmM.H5H0EHC
+
+        if ($userFromDB && password_verify($contraseña, $userFromDB->contraseña)) {
+            // Guardo en la sesión el ID del usuario
+            session_start();
+            session_regenerate_id(true); // Regenera el Id de la sesion para evitar ataques de fijacion de sesion
+            $_SESSION['ID_USER'] = $userFromDB->id;
+            $_SESSION['USERNAME'] = $userFromDB->usuario;
+            $_SESSION['LAST_ACTIVITY'] = time();
+
+            // Redirijo al home
+            header('Location: ' . BASE_URL);
+        } else {
+            return $this->view->mostrarLogin('Credenciales incorrectas');
+        }
     }
 
-    if (!isset($_POST['contraseña']) || empty($_POST['contraseña'])) {
-        return $this->view->mostrarLogin('Falta completar la contraseña');
+    public function logout()
+    {
+        session_start(); // Va a buscar la cookie
+        session_unset();
+        session_destroy(); // Borra la cookie que se buscó
+        header('Location:login');
+        exit();
     }
-
-    $usuario = $_POST['usuario'];
-    $contraseña = $_POST['contraseña'];
-
-    // Verificar que el usuario está en la base de datos
-    $userFromDB = $this->model->obtenerUsuario($usuario);
-
-    // password: 123456
-    // $userFromDB->password: $2y$10$xQop0wF1YJ/dKhZcWDqHceUM96S04u73zGeJtU80a1GmM.H5H0EHC
-    if($userFromDB && password_verify($contraseña, $userFromDB->contraseña)){
-        // Guardo en la sesión el ID del usuario
-        session_start();
-        $_SESSION['ID_USER'] = $userFromDB->id;
-        $_SESSION['USERNAME'] = $userFromDB->usuario;
-        $_SESSION['LAST_ACTIVITY'] = time();
-
-        // Redirijo al home
-        header('Location: ' . BASE_URL);
-     } else {
-        return $this->view->mostrarLogin('Credenciales incorrectas');
-     }
-  } 
-
-  public function logout() {
-    session_start(); // Va a buscar la cookie
-    session_destroy(); // Borra la cookie que se buscó
-    header('Location: ' . BASE_URL);
-    }
-
 }
